be7db5e026e3c1c9b4720627f56ea671e5dae16c
PON <cardasda@gmail.com>
2023-01-28 16:35:54 +0800

    EARNING TYPE AND EMPLOYEE EARNING - DONE

--- app/Models/Earning.php
+++ app/Models/Earning.php
@@ -9,7 +9,7 @@ class Earning extends Model
 {
     use HasFactory;
 
-    public function earningType(){
+    public function earning_type(){
         return $this->belongsTo(EarningType::class, "earning_type_id");
     }
 }
